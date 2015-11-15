ffdf233428d68cf281cabdb772735fcc71fc7f0f
jerelynco <jerelynco@gmail.com>
2015-11-15 13:00:33 +0800

    Added geocoder for MM cities collection

---
+++ geocoder.py
@@ -0,0 +1,23 @@
+import geocoder
+import json
+
+MM_cities = ('Manila', 'Caloocan', 'Las Pinas', 'Makati', 'Malabon', 'Mandaluyong', 'Marikina', 'Muntinlupa', 'Navotas', 'Paranaque', 'Pasay', 'Pasig', 'Pateros', 'Quezon City', 'San Juan', 'Taguig', 'Valenzuela')
+
+with open('MM_cities_collection.json', 'w') as outfile:
+	for i in range(len(MM_cities)):
+		g = geocoder.google(MM_cities[i]+', PH')
+		northeast_coord = g.geojson['bbox']['northeast']
+		southwest_coord = g.geojson['bbox']['southwest']
+
+		g_data[i] = {
+			'city_name': MM_cities[i], 
+			'bounds': [
+				{'lat': northeast_coord[0],'lon': southwest_coord[1]}, #upper left
+				{'lat': northeast_coord[0] ,'lon': northeast_coord[1]}, #upper right 
+				{'lat': southwest_coord[0],'lon': northeast_coord[1]}, #lower right
+				{'lat': southwest_coord[0],'lon': southwest_coord[1]} #lower left
+			]
+		}
+ 		
+ 		json.dump( g_data[i] , outfile)
+ 
